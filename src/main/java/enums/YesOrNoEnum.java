758708e3278f6e18469b664d76dcf748a4405a07
xujun <xujun@mistong.com>
2022-05-06 19:00:38 +0800

    1.初始化maven项目 2.增加YesOrNoEnum工具类

---
+++ src/main/java/enums/YesOrNoEnum.java
@@ -0,0 +1,59 @@
+package enums;
+
+import lombok.Getter;
+
+/**
+ * 常用枚举类型YES/NO
+ * 使用场景：常用于数据库某些只有0和1这两个值的字段
+ *
+ * @author xujun
+ * @version 1.0
+ */
+@Getter
+public enum YesOrNoEnum {
+    NO(0, "N", "否"),
+    YES(1, "Y", "是");
+
+    /**
+     * 数字编码
+     */
+    private final Integer code;
+    /**
+     * 文字编码
+     */
+    private final String characterCode;
+    /**
+     * 描述
+     */
+    private final String description;
+
+    YesOrNoEnum(Integer code, String characterCode, String description) {
+        this.code = code;
+        this.characterCode = characterCode;
+        this.description = description;
+    }
+
+    public static YesOrNoEnum getByCode(Integer code) {
+        if (code == null) {
+            return null;
+        }
+        for (YesOrNoEnum yesOrNoEnum : YesOrNoEnum.values()) {
+            if (yesOrNoEnum.code.equals(code)) {
+                return yesOrNoEnum;
+            }
+        }
+        return null;
+    }
+
+    public static YesOrNoEnum getByCharacterCode(String characterCode) {
+        if (characterCode == null) {
+            return null;
+        }
+        for (YesOrNoEnum yesOrNoEnum : YesOrNoEnum.values()) {
+            if (yesOrNoEnum.characterCode.equals(characterCode)) {
+                return yesOrNoEnum;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/test/java/Test.java
@@ -0,0 +1,8 @@
+/**
+ * 测试类
+ */
+public class Test {
+	public static void main(String[] args) {
+
+	}
+}
